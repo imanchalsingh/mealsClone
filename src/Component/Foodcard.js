5f6380ca26afb71ae302d5150c28f96e5b9f4db4
anchalDev159357 <singhanchal159357@gmail.com>
2023-10-31 16:56:40 +0530

    added changes

---
+++ src/Component/Foodcard.js
@@ -0,0 +1,198 @@
+import React, { useState } from "react";
+import logo from "./logopicture.png";
+import { meals } from "../foodapi";
+import { Button, Dialog, Grid } from "@mui/material";
+import { Favorite } from "@mui/icons-material";
+export default function Foodcard() {
+  const [data] = useState(meals);
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const [currentCardId, setCurrnetCardId] = useState("");
+  const open = () => {
+    setOpenDialog(true);
+  };
+  const close = () => {
+    setOpenDialog(false);
+  };
+
+  return (
+    <div>
+      <nav
+        style={{
+          backgroundColor: "#f0f0f5",
+          height: "60px",
+          display: "flex",
+          position: "fixed",
+          zIndex: "9999",
+          width: "100%",
+          transition: "all 0.3s ease-in-out",
+        }}
+      >
+        <img
+          style={{ width: "17%", height: "45px", marginTop: "3px" }}
+          src={logo}
+          alt="logo"
+        />
+      </nav>
+      <div style={{ backgroundColor: "#f0f0f5" }}>
+        <Grid container>
+          {data.map((items, idx) => {
+            return (
+              <Grid item xl={5} md={5} lg={5}>
+                <div
+                  key={idx}
+                  style={{
+                    backgroundColor: "#fff",
+                    boxShadow: "0px 0px 2px 0px gray",
+                    borderRadius: "20px",
+                    marginLeft: "180px",
+                    marginTop: "70px",
+                    height: "230px",
+                    display: "flex",
+                    width: "90%",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      paddingTop: "10px",
+                    }}
+                  >
+                    <div style={{ display: "flex" }}>
+                      <img
+                        style={{
+                          width: "40px",
+                          height: "40px",
+                          borderRadius: "50%",
+                          marginLeft: "10px",
+                        }}
+                        src={items.ImgUrl}
+                        alt=""
+                      />
+                      <p
+                        style={{
+                          fontSize: "20px",
+                          fontWeight: "bold",
+                          marginLeft: "10px",
+                        }}
+                      >
+                        {items.strName}
+                      </p>
+                    </div>
+                    <div>
+                      <Favorite
+                        sx={{
+                          color:
+                            openDialog && currentCardId === items.idMeal
+                              ? "red"
+                              : "black",
+
+                          fontSize: "30px",
+                          cursor: "pointer",
+                        }}
+                        onClick={() => {
+                          setCurrnetCardId(items.idMeal);
+                          open();
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div style={{ display: "flex", paddingBottom: "30px" }}>
+                    <img
+                      style={{
+                        width: "35%",
+                        height: "120px",
+                        borderRadius: "5px",
+                        boxShadow: "0px 0px 10px 0px gray",
+                        marginLeft: "-25px",
+                        backgroundColor: "#fff",
+                      }}
+                      src={items.strMealThumb}
+                      alt=""
+                    />
+
+                    <div>
+                      <p
+                        className="heading"
+                        style={{
+                          overflow: "hidden",
+                          fontSize: "23px",
+                          marginLeft: "15px",
+                        }}
+                      >
+                        {items.strMeal}
+                      </p>
+                      <p style={{ marginLeft: "15px", lineHeight: "0.2" }}>
+                        {Math.ceil(Math.random() * 10) + "M"} people watch.
+                      </p>
+                      <p style={{ marginLeft: "15px" }}>
+                        Uploaded {Math.ceil(Math.random() * 5)} hour ago.
+                      </p>
+                    </div>
+                  </div>
+                  <div style={{ display: "flex", justifyContent: "center" }}>
+                    <Button
+                      style={{
+                        backgroundColor: "#eb4715",
+                        color: "#fff",
+                        marginBottom: "15px",
+                        borderRadius: "50px",
+                        width: "107px",
+                        boxShadow: "0px 0px 10px 0px #eb4715",
+                      }}
+                    >
+                      Watch
+                    </Button>
+                    <Button
+                      style={{
+                        backgroundColor: "#eb4715",
+                        color: "#fff",
+                        marginBottom: "15px",
+                        marginLeft: "20px",
+                        borderRadius: "50px",
+                        width: "107px",
+                        boxShadow: "0px 0px 10px 0px #eb4715",
+                      }}
+                    >
+                      Repost
+                    </Button>
+                  </div>
+                </div>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </div>
+      {openDialog && (
+        <Dialog open={open}>
+          <div
+            style={{
+              width: "100%",
+              backgroundColor: "#eb4715",
+              padding: "30px",
+              textAlign: "center",
+              color: "#fff",
+            }}
+          >
+            <h3>Congratulations!</h3>
+            <p>Your meal is Add to favourite.</p>
+            <Button
+              onClick={close}
+              style={{
+                backgroundColor: "#fff",
+                color: "#000",
+                borderRadius: "50px",
+                width: "107px",
+              }}
+            >
+              Ok
+            </Button>
+          </div>
+        </Dialog>
+      )}
+    </div>
+  );
+}
---
+++ src/foodapi.js
@@ -0,0 +1,416 @@
+export const meals = [
+  {
+    strMeal: "Baked salmon with fennel & tomatoes",
+    strMealThumb: "https://www.themealdb.com/images/media/meals/1548772327.jpg",
+    idMeal: "52959",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Banks Medrano",
+  },
+  {
+    strMeal: "Escovitch Fish",
+    strMealThumb: "https://www.themealdb.com/images/media/meals/1520084413.jpg",
+    idMeal: "52944",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1694564717876-7436bdf6a236?auto=format&fit=crop&q=80&w=2005&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Lauren Orr",
+  },
+  {
+    strMeal: "Fish fofos",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/a15wsa1614349126.jpg",
+    idMeal: "53043",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1568467020043-d2982b8ec5f6?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Mac Lynn",
+  },
+  {
+    strMeal: "Fish pie",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/ysxwuq1487323065.jpg",
+    idMeal: "52802",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1635713150362-ed0cd425e697?auto=format&fit=crop&q=80&w=1915&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Lyla Richardson",
+  },
+  {
+    idMeal: "2",
+    strMeal: "Chicken",
+    strMealThumb: "https://www.themealdb.com/images/category/chicken.png",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1688895314096-17afac1f03d3?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Ridge Hill",
+  },
+  {
+    strMeal: "Garides Saganaki",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/wuvryu1468232995.jpg",
+    idMeal: "52764",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1674553048338-03f6a90a68df?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Eileen Brandt",
+  },
+  {
+    strMeal: "Grilled Portuguese sardines",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/lpd4wy1614347943.jpg",
+    idMeal: "53041",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1679753700871-ef1eddfbfd3f?auto=format&fit=crop&q=80&w=1956&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Graysen Camacho",
+  },
+  {
+    strMeal: "Honey Teriyaki Salmon",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/xxyupu1468262513.jpg",
+    idMeal: "52773",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1603988089669-c041ac2fe196?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Lily Benitez",
+  },
+  {
+    strMeal: "Kedgeree",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/utxqpt1511639216.jpg",
+    idMeal: "52887",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1581044777550-4cfa60707c03?auto=format&fit=crop&q=80&w=1886&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Leland Blanken",
+  },
+  {
+    strMeal: "Kung Po Prawns",
+    strMealThumb: "https://www.themealdb.com/images/media/meals/1525873040.jpg",
+    idMeal: "52946",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1651144159060-e4889dae080f?auto=format&fit=crop&q=80&w=1888&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Belen Hurley",
+  },
+  {
+    strMeal: "Laksa King Prawn Noodles",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/rvypwy1503069308.jpg",
+    idMeal: "52821",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1656534439580-19c4de76498b?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Lennon Medrano",
+  },
+  {
+    strMeal: "Mediterranean Pasta Salad",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/wvqpwt1468339226.jpg",
+    idMeal: "52777",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1694295166327-f6a24b69980d?auto=format&fit=crop&q=80&w=1923&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Navy Byrd",
+  },
+  {
+    strMeal: "Mee goreng mamak",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/xquakq1619787532.jpg",
+    idMeal: "53048",
+    ImgUrl:
+      "https://plus.unsplash.com/premium_photo-1665865607485-8eaaa827eea2?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Dillon Thomas",
+  },
+  {
+    strMeal: "Nasi lemak",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/wai9bw1619788844.jpg",
+    idMeal: "53051",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1698306799996-7b488dee2b6f?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Waverly Mills",
+  },
+  {
+    strMeal: "Portuguese fish stew",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/do7zps1614349775.jpg",
+    idMeal: "53045",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1697917225161-f43b4e60d2f4?auto=format&fit=crop&q=80&w=1811&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Orion Fry",
+  },
+  {
+    strMeal: "Recheado Masala Fish",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/uwxusv1487344500.jpg",
+    idMeal: "52809",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1697917983958-fec3e5252562?auto=format&fit=crop&q=80&w=1815&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Alondra Marin",
+  },
+  {
+    strMeal: "Salmon Avocado Salad",
+    strMealThumb: "https://www.themealdb.com/images/media/meals/1549542994.jpg",
+    idMeal: "52960",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1682917265602-cda116a64684?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Salem Sosa",
+  },
+  {
+    strMeal: "Salmon Prawn Risotto",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/xxrxux1503070723.jpg",
+    idMeal: "52823",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1697725351522-7d63b2e64698?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Aisha Barker",
+  },
+  {
+    strMeal: "Saltfish and Ackee",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/vytypy1511883765.jpg",
+    idMeal: "52936",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1697396431604-0648ed5cf56d?auto=format&fit=crop&q=80&w=1885&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Dillon Harper",
+  },
+  {
+    strMeal: "Seafood fideu\u00e0",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/wqqvyq1511179730.jpg",
+    idMeal: "52836",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1674847059298-765afa839ec7?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Corinne Harris",
+  },
+  {
+    strMeal: "Shrimp Chow Fun",
+    strMealThumb: "https://www.themealdb.com/images/media/meals/1529445434.jpg",
+    idMeal: "52953",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1689193504709-2ccf8bf64be1?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Maddox Avalos",
+  },
+  {
+    strMeal: "Sledz w Oleju (Polish Herrings)",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/7ttta31593350374.jpg",
+    idMeal: "53023",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1495846101638-bd8e9a23f8ee?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Stella Ayala",
+  },
+  {
+    strMeal: "Spring onion and prawn empanadas",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/1c5oso1614347493.jpg",
+    idMeal: "53040",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1542596594-649edbc13630?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Fox McIntosh",
+  },
+  {
+    strMeal: "Sushi",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/g046bb1663960946.jpg",
+    idMeal: "53065",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Jenna Nelson",
+  },
+  {
+    strMeal: "Three Fish Pie",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/spswqs1511558697.jpg",
+    idMeal: "52882",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1611695434369-a8f5d76ceb7b?auto=format&fit=crop&q=80&w=1935&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Jaime McDowell",
+  },
+  {
+    strMeal: "Tuna and Egg Briks",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/2dsltq1560461468.jpg",
+    idMeal: "52975",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1512646605205-78422b7c7896?auto=format&fit=crop&q=80&w=1935&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Mercy Cherry",
+  },
+  {
+    strMeal: "Tuna Nicoise",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/yypwwq1511304979.jpg",
+    idMeal: "52852",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1521146764736-56c929d59c83?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Maxton Andrews",
+  },
+  {
+    idMeal: "1",
+    strMeal: "Beef",
+    strMealThumb: "https://www.themealdb.com/images/category/beef.png",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1516195851888-6f1a981a862e?auto=format&fit=crop&q=80&w=1905&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Brielle Nava",
+  },
+  {
+    idMeal: "3",
+    strMeal: "Dessert",
+    strMealThumb: "https://www.themealdb.com/images/category/dessert.png",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Ronin Hines",
+  },
+  {
+    strMeal: "Cajun spiced fish tacos",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg",
+    idMeal: "52819",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?auto=format&fit=crop&q=80&w=1964&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Albert Dera",
+  },
+  {
+    idMeal: "4",
+    strMeal: "Lamb",
+    strMealThumb: "https://www.themealdb.com/images/category/lamb.png",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1499155286265-79a9dc9c6380?auto=format&fit=crop&q=80&w=1884&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Leyla Pennington",
+  },
+  {
+    idMeal: "5",
+    strMeal: "Miscellaneous",
+    strMealThumb: "https://www.themealdb.com/images/category/miscellaneous.png",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1542596768-5d1d21f1cf98?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Nathaniel House",
+  },
+  {
+    idMeal: "6",
+    strMeal: "Pasta",
+    strMealThumb: "https://www.themealdb.com/images/category/pasta.png",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1544168190-79c17527004f?auto=format&fit=crop&q=80&w=1888&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Remi Casey",
+  },
+  {
+    idMeal: "7",
+    strMeal: "Pork",
+    strMealThumb: "https://www.themealdb.com/images/category/pork.png",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1517256673644-36ad11246d21?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Cash Shepard",
+  },
+  {
+    strMeal: "Fish Stew with Rouille",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/vptqpw1511798500.jpg",
+    idMeal: "52918",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1589156191108-c762ff4b96ab?auto=format&fit=crop&q=80&w=1886&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Rayne Vaughn",
+  },
+  {
+    idMeal: "8",
+    strMeal: "Seafood",
+    strMealThumb: "https://www.themealdb.com/images/category/seafood.png",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1557053910-d9eadeed1c58?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Briar Hunter",
+  },
+  {
+    idMeal: "9",
+    strMeal: "Side",
+    strMealThumb: "https://www.themealdb.com/images/category/side.png",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1529068755536-a5ade0dcb4e8?auto=format&fit=crop&q=80&w=1781&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Rylie Suarez",
+  },
+  {
+    idMeal: "10",
+    strMeal: "Starter",
+    strMealThumb: "https://www.themealdb.com/images/category/starter.png",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1548366565-6bbab241282d?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Martin Paul",
+  },
+  {
+    idMeal: "11",
+    strMeal: "Vegan",
+    strMealThumb: "https://www.themealdb.com/images/category/vegan.png",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1548142813-c348350df52b?auto=format&fit=crop&q=80&w=1889&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Blakely Lin",
+  },
+  {
+    idMeal: "12",
+    strMeal: "Vegetarian",
+    strMealThumb: "https://www.themealdb.com/images/category/vegetarian.png",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1539125530496-3ca408f9c2d9?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Alonso Wilcox",
+  },
+  {
+    idMeal: "13",
+    strMeal: "Breakfast",
+    strMealThumb: "https://www.themealdb.com/images/category/breakfast.png",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1567532939604-b6b5b0db2604?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Adelina Perez",
+  },
+  {
+    idMeal: "14",
+    strMeal: "Goat",
+    strMealThumb: "https://www.themealdb.com/images/category/goat.png",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?auto=format&fit=crop&q=80&w=1727&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Kian Barber",
+  },
+
+  {
+    idMeal: "52768",
+    strMeal: "Apple Frangipan Tart",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Amayah Shannon",
+  },
+  {
+    idMeal: "52893",
+    strMeal: "Apple & Blackberry Crumble",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/xvsurr1511719182.jpg",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?auto=format&fit=crop&q=80&w=2048&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Isaias Hood",
+  },
+  {
+    idMeal: "53049",
+    strMeal: "Apam balik",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Keilani Chandler",
+  },
+  {
+    idMeal: "53050",
+    strMeal: "Ayam Percik",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/020z181619788503.jpg",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1541823709867-1b206113eafd?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Jeremy Glover",
+  },
+  {
+    idMeal: "53034",
+    strMeal: "Japanese Katsudon",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/d8f6qx1604182128.jpg",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1580489944761-15a19d654956?auto=format&fit=crop&q=80&w=1961&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Mercy Ferguson",
+  },
+  {
+    idMeal: "52771",
+    strMeal: "Spicy Arrabiata Penne",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
+    ImgUrl:
+      "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?auto=format&fit=crop&q=80&w=2071&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    strName: "Yasmin Trujillo",
+  },
+];
